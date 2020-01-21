86296afc49adbefe1d3a653eaec844a9b5e722b9
Pratik Patra <pratikpatra95@outlook.com>
2020-01-22 01:32:29 +0530

    Hand Cricket V0.3 - Refactors

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,7 @@ fn receive_validate_input() ->u32{
     return player_intended_input;
 }
 
-fn main() {
-    println!("Welcome to Hand Cricket game!");
-
-    println!("You get to bat first! Your innings starts now!");
-
-    let mut player_runs = 0;
-    let mut computer_runs = 0;
+fn player_batting(player_runs : &mut u32, computer_runs : &mut u32){
     loop{
         let player_intended_run = receive_validate_input();
         if player_intended_run == 0{
@@ -34,18 +28,17 @@ fn main() {
         println!("Computer entered : {}",computer_number);
         match player_intended_run.cmp(&computer_number){
             Ordering::Equal => {
-                println!("That's out! Computer needs {} to win", player_runs+1);
+                println!("That's out! Computer needs {} to win", *player_runs+1);
                 break;
             }
             _ => {
-                player_runs+=player_intended_run;
-                println!("Runs : {}", player_runs);
+                *player_runs+=player_intended_run;
+                println!("Runs : {}", *player_runs);
             }
         }
     }
-    println!("=======End of Innings=====");
-    println!("You get to bowl now!");
-    println!("Computer needs {} to win", player_runs+1);
+}
+fn player_bowling(player_runs : &mut u32, computer_runs : &mut u32){
     loop{
         let player_intended_ball = receive_validate_input();
         let computer_intended_run = rand::thread_rng().gen_range(1,7);
@@ -53,19 +46,32 @@ fn main() {
 
         match computer_intended_run.cmp(&player_intended_ball){
             Ordering::Equal => {
-                println!("That's out! Computer score : {} You win by {} runs", computer_runs, player_runs-computer_runs+1);
+                println!("That's out! Computer score : {} You win by {} runs", computer_runs, *player_runs-*computer_runs+1);
                 break;
             }
             _ => {
-                computer_runs += computer_intended_run;
-                if computer_runs > player_runs + 1{
+                *computer_runs += computer_intended_run;
+                if *computer_runs > *player_runs + 1{
                     println!("Computer wins! Better luck next time!");
                     break;
                 }
                 else{
-                    println!("Computer Runs : {}, Computer needs {} runs to win", computer_runs, player_runs-computer_runs+1);
+                    println!("Computer Runs : {}, Computer needs {} runs to win", *computer_runs, *player_runs-*computer_runs+1);
                 }
             }
         }
     }
+}
+fn main() {
+    println!("Welcome to the Hand Cricket game!");
+
+    println!("You get to bat first! Your innings starts now!");
+
+    let mut player_runs:u32 = 0;
+    let mut computer_runs:u32 = 0;
+    player_batting(&mut player_runs,&mut computer_runs);
+    println!("=======End of Innings=====");
+    println!("You get to bowl now!");
+    println!("Computer needs {} to win", player_runs+1);
+    player_bowling(&mut player_runs, &mut computer_runs);
 }
